73735dbafbf90a95c85cc0dc4da7e33cfb992724
Louis Dégardin <louis.degardin@protonmail.com>
2022-10-05 22:38:37 +0200

    button enable/disable, update gui on track selection

--- src/main/java/com/example/orkestro/MainController.java
+++ src/main/java/com/example/orkestro/MainController.java
@@ -5,10 +5,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -21,6 +18,12 @@ import java.util.List;
 import java.util.Map;
 
 public class MainController {
+
+    @FXML
+    public Button playBtn;
+
+    @FXML
+    public Button stopBtn;
 
     @FXML
     private VBox tracksPane = new VBox();
@@ -43,6 +46,36 @@ public class MainController {
     @FXML
     public void initialize() {
         tracksListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        tracksListView.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            // enable buttons
+            if(newValue != null){
+                playBtn.setDisable(false);
+                stopBtn.setDisable(false);
+            }
+
+            // build medias map
+            File tracksDir = new File(BASE_DIR + File.separator + groupListView.getSelectionModel().getSelectedItem()
+                    + File.separator + tracksListView.getSelectionModel().getSelectedItem());
+            medias.clear();
+            try {
+                for (File trackFile : tracksDir.listFiles()) {
+                    //getClass().getResource("tracks/gojira/Sphinx/08 Sphinx_bass_mixed.mp3")
+                    // use "/" because Media doesn't accept "\"
+                    Media media = new Media(getClass().getResource(TRACKS_DIRNAME + "/" + groupListView.getSelectionModel().getSelectedItem() + "/"
+                            + tracksListView.getSelectionModel().getSelectedItem() + "/" + trackFile.getName()).toURI().toString());
+                    medias.put(trackFile.getName(), new MediaPlayer(media));
+                }
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+
+            // update sliders
+            tracksPane.getChildren().clear();
+            for (Map.Entry<String, MediaPlayer> entry : medias.entrySet()) {
+                tracksPane.getChildren().add(new Label(entry.getKey()));
+                tracksPane.getChildren().add(buildSlider(entry.getValue()));
+            }
+        });
 
         groupListView.setItems(initgroups());
         groupListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
@@ -89,33 +122,9 @@ public class MainController {
     @FXML
     protected void onPlayClick()
     {
-        // build medias map
-        File tracksDir = new File(BASE_DIR + File.separator + groupListView.getSelectionModel().getSelectedItem()
-                + File.separator + tracksListView.getSelectionModel().getSelectedItem());
-        medias.clear();
-        try {
-            for (File trackFile : tracksDir.listFiles()) {
-                //getClass().getResource("tracks/gojira/Sphinx/08 Sphinx_bass_mixed.mp3")
-                // use "/" because Media doesn't accept "\"
-                Media media = new Media(getClass().getResource(TRACKS_DIRNAME + "/" + groupListView.getSelectionModel().getSelectedItem() + "/"
-                        + tracksListView.getSelectionModel().getSelectedItem() + "/" + trackFile.getName()).toURI().toString());
-                medias.put(trackFile.getName(), new MediaPlayer(media));
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        // play
         for (MediaPlayer mediaplayer: medias.values()) {
             mediaplayer.setVolume(DEFAULT_VOLUME);
             mediaplayer.play();
-        }
-
-        // update sliders
-        tracksPane.getChildren().clear();
-        for (Map.Entry<String, MediaPlayer> entry : medias.entrySet()) {
-            tracksPane.getChildren().add(new Label(entry.getKey()));
-            tracksPane.getChildren().add(buildSlider(entry.getValue()));
         }
     }
 
