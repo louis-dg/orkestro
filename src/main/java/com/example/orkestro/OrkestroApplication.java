c691859ebf40d69ebe355c8c848b792b6143f630
Louis Dégardin <louis.degardin@protonmail.com>
2022-10-09 11:36:21 +0200

    spacing

--- src/main/java/com/example/orkestro/OrkestroApplication.java
+++ src/main/java/com/example/orkestro/OrkestroApplication.java
@@ -11,7 +11,7 @@ public class OrkestroApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(OrkestroApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Orkestro");
         stage.setScene(scene);
         stage.show();
