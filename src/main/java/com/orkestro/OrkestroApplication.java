a54a4501d855e71fe53ffc975356ff27fbc7784c
Louis Dégardin <louis.degardin@protonmail.com>
2022-10-22 19:25:55 +0200

    Add stage icon

--- src/main/java/com/orkestro/OrkestroApplication.java
+++ src/main/java/com/orkestro/OrkestroApplication.java
@@ -3,9 +3,11 @@ package com.orkestro;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -14,11 +16,12 @@ public class OrkestroApplication extends Application {
     private static Locale locale = Locale.getDefault();
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, URISyntaxException {
         Logs.getLogger().info("Starting Orkestro application");
         FXMLLoader fxmlLoader = new FXMLLoader(OrkestroApplication.class.getResource("main-view.fxml"));
         fxmlLoader.setResources(ResourceBundle.getBundle("com.orkestro.i18n.orkestro", locale));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(getClass().getResource("music-solid.png").toURI().toURL().toString()));
         stage.setTitle("Orkestro");
         stage.setScene(scene);
         stage.show();
